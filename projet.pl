3ba8b87d171e86a45ab343512d82b1e1b47d2d6e
NicolasSzewe <nicolas.szewe@gmail.com>
2014-06-03 09:18:57 +0200

    Fonction de deplacement d'une graine et d'affichage d'une liste

--- projet.pl
+++ projet.pl
@@ -21,21 +21,28 @@ tourjoueur(X):- joueur(X),
 		retract(joueur(j2)),
 		asserta(joueur(j1)).
 
+autrejoueur(Y):- joueur(Y).
+
+
 %--------------------------------------------------
 
 
 %Ici nous placons toute nos fonctions dites usuelle ou de base
 
-% Pour imprimer une liste
+% Fonction d'affichage
 imprime_liste([]).
 imprime_liste([T|Q]):- write(T),write('-'),imprime_liste(Q).
+
+affichage(Joueur,Liste):- write(Joueur),write(' : '),imprime_liste(Liste).
+
+
 
 %Pour récupérer un element en position X dans une liste L 
 prendre(L,X,R):- nth0(X,L,R).	
 
 
 %Pour modifier l'objet en position Position de la liste Joueur avec la valeur Valeur, et la nouvelle liste est retournée dans NouveauJoueur
-modifier(Joueur,Liste,Position,Valeur,NouvelleListe):-  
+modifier(Liste,Position,Valeur,NouvelleListe):-  
    	 length(Temporaire, Position),
     	 append(Temporaire, [_|Tail], Liste),
     	 append(Temporaire, [Valeur|Tail], NouvelleListe).
@@ -44,31 +51,42 @@ modifier(Joueur,Liste,Position,Valeur,NouvelleListe):-
 
 %--------------------------------------------------------------------
 
+% Pour déplacer une graine d'un état i à un état i+1 
+
+deplacer(Joueur,Liste,Position,PositionDepart,R1):- 
+		prendre(Liste,Position,Value),
+		prendre(Liste,PositionDepart,R),
+		Value1 is Value+1,
+		modifier(Liste,Position,Value1,Temp),
+		R1 is R-1,
+		modifier(Temp,PositionDepart,R1,NouvelleListe),
+		retract(tablejoueur(Joueur,Liste)),
+ 		asserta(tablejoueur(Joueur,NouvelleListe)).
+ 		
+
+
 
 % Mise à jour de la table en fonction de l'action demandé par l'utilisateur
 
-majtable(Joueur,PositionDepart,PositionAvancer) :-	tablejoueur(Joueur, Liste),
+majtable(Joueur,Autre,PositionDepart,PositionAvancer) :-	
+							tablejoueur(Joueur, Liste),
+							tablejoueur(Autre,ListeAutre),
 							prendre(Liste,PositionDepart,R),
-							write('Objet a la position demande : '),write(R),nl,
 							NouvellePosition is PositionAvancer+1,
 							R > 0 ->
 							(
-								prendre(Liste,NouvellePosition,Value),
-								Value1 is Value+1,
-								modifier(Joueur,Liste,NouvellePosition,Value1,Temp),
-								R1 is R-1,
-								modifier(Joueur,Temp,PositionDepart,R1,NouvelleListe),
-								retract(tablejoueur(Joueur,Liste)),
- 								asserta(tablejoueur(Joueur,NouvelleListe)),
- 								R1 > 0 ->
-								majtable(Joueur,PositionDepart,NouvellePosition);
-								write('')
+								R < 5 -> (
+								deplacer(Joueur,Liste,NouvellePosition,PositionDepart,R1);
+								write('')),
+								R1 > 0 ->(
+								majtable(Joueur,PositionDepart,NouvellePosition); write(' '))
 							).
 
 
 %Interrogation Joueur
 
 jouer:- 	tourjoueur(X),
+		autrejoueur(Y),
 		tablejoueur(X,L),
 		write('Tour de : '),write(X),nl,
 		imprime_liste(L),nl,
@@ -76,9 +94,9 @@ jouer:- 	tourjoueur(X),
 		write('Choisir une position '),nl,
 		read(C),
 		C1 is C-1,
-		majtable(X,C1,C1),
+		majtable(X,Y,C1,C1),
 		tablejoueur(X,L2),
-		imprime_liste(L2),nl,nl,nl,jouer.
+		affichage(X,L2),nl,nl,nl,jouer.
 
 
 % Fonction principale du projet
