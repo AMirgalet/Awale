7ddf6cd081d6e86b132c6ce87140a49c0cefd29a
sami <sami.yacoubi@hotmail.fr>
2014-06-04 15:33:56 +0200

    Save pour le changement de val

--- projet.pl
+++ projet.pl
@@ -1,6 +1,6 @@
 % Une fonction d'initialisation du plateau de jeu
-init:-	   asserta(tablejoueur(j1,[4, 4, 4, 4, 4, 4]) ),
-		asserta(tablejoueur(j2,[4, 4, 4, 4, 4, 4]) ),
+init:-	   asserta(tablejoueur(j1,[4, 4, 4, 4, 4, 4],0) ),
+		asserta(tablejoueur(j2,[4, 4, 4, 4, 4, 4],0) ),
 	   asserta(joueur(j1)).
 
 victoire(S):- S>=25.
@@ -37,11 +37,11 @@ imprime_liste_inversee(L):-inverse(L,L1), imprime_liste(L1).
 
 %affichage(Joueur,Liste):- write(Joueur),write(' : '),imprime_liste(Liste).
 
-affichage(JoueurUn, ListeUn, JoueurDeux, ListeDeux):-
+affichage(JoueurUn, ListeUn, PtsUn, JoueurDeux, ListeDeux, PtsDeux):-
 				write('  ______________'),nl,
-				write(JoueurDeux),write('|'),imprime_liste_inversee(ListeDeux),write('|'),nl,
+				write(JoueurDeux),write('|'),imprime_liste_inversee(ListeDeux),write('|'),write('Score: '),write(PtsDeux),nl,
 				write('  |____________|'),nl,
-				write(JoueurUn),write('|'),imprime_liste(ListeUn),write('|'),nl,
+				write(JoueurUn),write('|'),imprime_liste(ListeUn),write('|'),write('Score: '),write(PtsUn),nl,
 				write('  |____________|'),nl.
 
 
@@ -68,15 +68,19 @@ modifier(Liste,Position,Valeur,NouvelleListe):-
 
 deplacer(Joueur,Liste,Position,PositionDepart,R1):- 
 		prendre(Liste,Position,Value),
+		%Value est la valeur de la case a laquelle on ajoute 1
 		prendre(Liste,PositionDepart,R),
+		%R est la valeur actuelle de la case de départ
 		Value1 is Value+1,
 		modifier(Liste,Position,Value1,Temp),
+		%Temp est la nouvelle liste modifiee
 		R1 is R-1,
 		%write('R :'),write(R),nl,
 		%write('R1 : '),write(R1),nl,
 		modifier(Temp,PositionDepart,R1,NouvelleListe),
-		retract(tablejoueur(Joueur,Liste)),
- 		asserta(tablejoueur(Joueur,NouvelleListe)).
+		%NouvelleListe est la nouvelle liste modifiee
+		retract(tablejoueur(Joueur,Liste, Points)),
+ 		asserta(tablejoueur(Joueur,NouvelleListe, Points)).
  		
 
 
@@ -84,14 +88,32 @@ deplacer(Joueur,Liste,Position,PositionDepart,R1):-
 % Mise à jour de la table en fonction de l'action demandé par l'utilisateur
 
 majtable(Joueur,Autre,PositionDepart,PositionAvancer) :-	
-							tablejoueur(Joueur, Liste),
-							tablejoueur(Autre,ListeAutre),
+							tablejoueur(Joueur, Liste, PointsUn),
+							tablejoueur(Autre,ListeAutre, PointsDeux),
 							prendre(Liste,PositionDepart,R),
+							%R est la valeur de la case choisie
 							NouvellePosition is PositionAvancer+1,
+							%On commence le changement la
+							NouvellePosition > 5 ->
+							(
+								R > 0 ->
+								(
+									deplacer(Joueur,Liste,NouvellePosition,PositionDepart,R1),
+									R1 > 0 ->
+									(
+									%write('R1 : '),write(R1),nl,
+									majtable(Joueur,Autre,PositionDepart,NouvellePosition);
+									write('')
+									)
+								)
+							)
+							write('NvlPos :'),write(NouvellePosition),nl,
 							R > 0 ->
 							(
 								deplacer(Joueur,Liste,NouvellePosition,PositionDepart,R1),
+								
  								R1 > 0 ->
+								%write('R1 : '),write(R1),nl,
 								majtable(Joueur,Autre,PositionDepart,NouvellePosition);
 								write('')
 							).
@@ -100,16 +122,16 @@ majtable(Joueur,Autre,PositionDepart,PositionAvancer) :-
 
 jouer:- 	tourjoueur(X),
 		autrejoueur(Y),
-		tablejoueur(X,L),
-		tablejoueur(Y,Y2),
-		affichage(X,L,Y,Y2),nl,
+		tablejoueur(X,L,P),
+		tablejoueur(Y,Y2,P2),
+		affichage(X,L,P,Y,Y2,P2),nl,
 		write('Tour de : '),write(X),nl,
 		write('Choisir une position '),nl,
 		read(C),
 		C1 is C-1,
 		majtable(X,Y,C1,C1),
-		tablejoueur(X,L2),
-		tablejoueur(Y,Y2),
+		tablejoueur(X,L2,P),
+		tablejoueur(Y,Y2,P2),
 		%affichage(X,L2,Y,Y2),nl,
 		
 		jouer.
