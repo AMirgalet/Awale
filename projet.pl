cd4502b394ff3d1d78235e4b965b28c4a302a960
sami <sami.yacoubi@hotmail.fr>
2014-06-04 19:13:49 +0200

    Distribution sur le plateau adverse
    Normalement tout fonctionne, par contre il y a toujours un problème, de
    temps en temps le joueur 1 peut jouer deux fois de suite en début de
    partie. A regarder

--- projet.pl
+++ projet.pl
@@ -1,7 +1,7 @@
 % Une fonction d'initialisation du plateau de jeu
-init:-	   asserta(tablejoueur(j1,[4, 4, 4, 4, 4, 4],0) ),
+init:-	asserta(tablejoueur(j1,[4, 4, 4, 4, 4, 12],0) ),
 		asserta(tablejoueur(j2,[4, 4, 4, 4, 4, 4],0) ),
-	   asserta(joueur(j1)).
+	    asserta(joueur(j1)).
 
 victoire(S):- S>=25.
 
@@ -64,9 +64,11 @@ modifier(Liste,Position,Valeur,NouvelleListe):-
 
 %--------------------------------------------------------------------
 
-% Pour déplacer une graine d'un état i à un état i+1 
+% Pour déplacer une graine d'un état i à un état i+1 dans une seule chaine
 
-deplacer(Joueur,Liste,Position,PositionDepart,R1):- 
+deplacer(Joueur,Autre,Position,PositionDepart,R1):- 
+		tablejoueur(Joueur, Liste, PointsUn),
+		tablejoueur(Autre,ListeAutre, PointsDeux),
 		prendre(Liste,Position,Value),
 		%Value est la valeur de la case a laquelle on ajoute 1
 		prendre(Liste,PositionDepart,R),
@@ -75,14 +77,30 @@ deplacer(Joueur,Liste,Position,PositionDepart,R1):-
 		modifier(Liste,Position,Value1,Temp),
 		%Temp est la nouvelle liste modifiee
 		R1 is R-1,
-		%write('R :'),write(R),nl,
-		%write('R1 : '),write(R1),nl,
 		modifier(Temp,PositionDepart,R1,NouvelleListe),
 		%NouvelleListe est la nouvelle liste modifiee
 		retract(tablejoueur(Joueur,Liste, Points)),
  		asserta(tablejoueur(Joueur,NouvelleListe, Points)).
- 		
-
+		
+% Pour déplacer une graine d'un état i à un état i+1 dans les deux chaines
+	
+deplacer_deux_listes(Joueur,Autre,Position,PositionDepart,R1):- 
+		tablejoueur(Joueur, Liste, PointsUn),
+		tablejoueur(Autre,ListeAutre, PointsDeux),
+		prendre(ListeAutre,Position,Value),
+		%Value est la valeur de la case a laquelle on ajoute 1
+		prendre(Liste,PositionDepart,R),
+		%R est la valeur actuelle de la case de départ
+		Value1 is Value+1,
+		modifier(ListeAutre,Position,Value1,Temp),
+		%Temp est la nouvelle liste modifiee
+		R1 is R-1,
+		modifier(Liste,PositionDepart,R1,NouvelleListe),
+		%NouvelleListe est la nouvelle liste modifiee
+		retract(tablejoueur(Joueur,Liste, PointsUn)),
+ 		asserta(tablejoueur(Joueur,NouvelleListe, PointsUn)),
+		retract(tablejoueur(Autre,ListeAutre, PointsDeux)),
+ 		asserta(tablejoueur(Autre,Temp, PointsDeux)).
 
 
 % Mise à jour de la table en fonction de l'action demandé par l'utilisateur
@@ -93,29 +111,34 @@ majtable(Joueur,Autre,PositionDepart,PositionAvancer) :-
 							prendre(Liste,PositionDepart,R),
 							%R est la valeur de la case choisie
 							NouvellePosition is PositionAvancer+1,
-							%On commence le changement la
-							NouvellePosition > 5 ->
-							(
-								R > 0 ->
-								(
-									deplacer(Joueur,Liste,NouvellePosition,PositionDepart,R1),
-									R1 > 0 ->
-									(
-									%write('R1 : '),write(R1),nl,
-									majtable(Joueur,Autre,PositionDepart,NouvellePosition);
-									write('')
+							%Premiere condition pour gérer les cas ou on repasse sur la case qui distribue
+							(NouvellePosition = PositionDepart ->
+								majtable(Joueur,Autre,PositionDepart,NouvellePosition);
+								%Deuxième condition qui vérifie si que l'on retourne sur le plateau du joueur
+								%actuel si on a beaucoup de jetons
+								(NouvellePosition > 11 ->
+									PositionTemp is NouvellePosition -13,
+									majtable(Joueur,Autre,PositionDepart,PositionTemp);
+									%Troisieme condition qui vérifie si l'on distribue sur le plateau de l'adversaire
+									(NouvellePosition > 5 ->
+										(R > 0 ->
+										
+											PositionChaineDeux is NouvellePosition-6,
+											deplacer_deux_listes(Joueur,Autre,PositionChaineDeux,PositionDepart,R1),
+											(R1 > 0 ->
+												majtable(Joueur,Autre,PositionDepart,NouvellePosition);
+												write('')
+											)
+										;write(''))
+										;(R > 0 ->
+											deplacer(Joueur,Autre,NouvellePosition,PositionDepart,R1),
+											(R1 > 0 ->
+												majtable(Joueur,Autre,PositionDepart,NouvellePosition);
+												write('')
+											)
+										;write(''))
 									)
 								)
-							)
-							write('NvlPos :'),write(NouvellePosition),nl,
-							R > 0 ->
-							(
-								deplacer(Joueur,Liste,NouvellePosition,PositionDepart,R1),
-								
- 								R1 > 0 ->
-								%write('R1 : '),write(R1),nl,
-								majtable(Joueur,Autre,PositionDepart,NouvellePosition);
-								write('')
 							).
 
 %Interrogation Joueur
@@ -130,9 +153,6 @@ jouer:- 	tourjoueur(X),
 		read(C),
 		C1 is C-1,
 		majtable(X,Y,C1,C1),
-		tablejoueur(X,L2,P),
-		tablejoueur(Y,Y2,P2),
-		%affichage(X,L2,Y,Y2),nl,
 		
 		jouer.
 
